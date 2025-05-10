fe55f8f189b6777122da67a9b472a6b110e64ba1
Artem <cartem33@gmail.com>
2025-05-10 17:07:11 +0300

    1.6. Взаимодействие объектов [#166087 #410660]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,27 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
         String voice = "may - may";
         return voice;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
     }
 
     public static void main(String[] args) {
@@ -11,5 +29,16 @@ public class Cat {
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says: " + say);
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("cutlet");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
+        System.out.println("There is sparky`s food.");
+        sparky.eat("sosij");
+        sparky.show();
     }
 }
