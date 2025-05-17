419c23adcda551717b8dc9058fa85ea0f5ea96bd
Artem <cartem33@gmail.com>
2025-05-17 16:22:07 +0300

    6.0. Абстрактные классы [#504949 #411516]

---
+++ src/main/java/ru/job4j/oop/Truck.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Truck extends Vehicle {
+
+    public void accelerate() {
+        System.out.println("Электронная педаль газа");
+    }
+
+    public void steer() {
+        System.out.println("Стандартное рулевое управление");
+    }
+}
